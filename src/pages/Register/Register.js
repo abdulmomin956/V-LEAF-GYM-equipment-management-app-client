96492e3ed6f2489094f868b2ceef6d43f52eb018
Abdul <abdulmomin956@gmail.com>
2022-05-07 13:17:49 +0600

    createuserwith email pass auth

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passRef = useRef()
     const handleOnSubmit = e => {
         e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passRef.current.value;
+        console.log(name, email, password);
+        createUserWithEmailAndPassword(email, password);
 
     }
     return (
@@ -13,16 +32,16 @@ const Register = () => {
                 <Form onSubmit={handleOnSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Full name</Form.Label>
-                        <Form.Control type="text" placeholder="Full name" />
+                        <Form.Control ref={nameRef} type="text" placeholder="Full name" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control ref={passRef} type="password" placeholder="Password" required />
                     </Form.Group>
                     <Button className='bg-myViolet text-white shadow-none' type="submit">
                         Register
