cd92ec87fc2f226a6ea0287acd3594e2c03bd170
Abdul <abdulmomin956@gmail.com>
2022-05-08 16:08:52 +0600

    delete function added

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import useInventory from '../../shared/CustomHook/useInventory';
-import Item from '../Item/Item';
 import './Inventory.css'
 
 const Inventory = () => {
     const [products, setProducts] = useInventory();
+
+    const handleDelete = id => {
+        fetch('http://localhost:5000/delete/' + id, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                const remaining = products.filter(service => service._id !== id);
+                setProducts(remaining);
+            })
+    }
+
     return (
         <div>
-            <h1 className='text-center'>Manage Inventory</h1>
+            <h1 className='text-center'>Manage Inventory ({products.length})</h1>
             <table className='mx-auto styled-table'>
                 <thead>
                     <tr>
@@ -20,7 +33,13 @@ const Inventory = () => {
                 </thead>
                 <tbody>
                     {
-                        products.map(product => <Item key={product._id} product={product}></Item>)
+                        products.map(product => <tr key={product._id} product={product}>
+                            <td><img height='40px' src={product.image} alt="" /></td>
+                            <td>{product.name}</td>
+                            <td>{product.price} Taka</td>
+                            <td>{product.qty} pcs</td>
+                            <td className='position-relative'><p>{product.suppName}</p><Button onClick={() => handleDelete(product._id)} className='bg-myViolet text-white shadow-none d-block mx-auto' style={{ position: 'absolute', right: '10px', top: '20px' }}>delete</Button></td>
+                        </tr>)
                     }
                 </tbody>
             </table>
--- src/pages/Update/Update.js
+++ src/pages/Update/Update.js
@@ -1,17 +1,62 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import './Update.css'
 
 const Update = () => {
     const { id } = useParams()
+    const [product, setProduct] = useState({});
+
+    const handleOnAdd = e => {
+        e.preventDefault()
+    }
+
     useEffect(() => {
-        const url = `http://localhost:5000/product/${id}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => console.log(data))
+        const getData = async () => {
+            const url = `http://localhost:5000/product/${id}`
+            await fetch(url)
+                .then(res => res.json())
+                .then(data => setProduct(data))
+        }
+        getData();
+
     }, [id])
+    // console.log(product);
+    const { _id, name, short, price, qty, suppName, image } = product;
     return (
-        <div>
-            {id}
+        <div className='text-center'>
+            <h1>{_id}</h1>
+            <img className='d-block w-25 mx-auto' src={image} alt="" />
+            <h3>{name}</h3>
+
+            <table className='mx-auto item-table'>
+                <tr>
+                    <th>Price:</th>
+                    <td>{price}</td>
+                </tr>
+                <tr>
+                    <th>Description:</th>
+                    <td>{short}</td>
+                </tr>
+                <tr>
+                    <th>QTY:</th>
+                    <td>{qty} pcs</td>
+                </tr>
+                <tr>
+                    <th>Sold:</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Supplier:</th>
+                    <td>{suppName}</td>
+                </tr>
+            </table>
+            <button>Delivered</button>
+            <form onSubmit={handleOnAdd}>
+                <input type="number" min="1" placeholder='QTY' /><button>Restock</button>
+            </form>
+
+            <Link className='text-myViolet' to='/inventory'><Button className='bg-myViolet text-white shadow-none d-block mx-auto mt-3'>Manage Inventory</Button></Link>
         </div>
     );
 };
